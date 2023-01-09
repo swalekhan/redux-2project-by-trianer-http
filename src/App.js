93bf415d9236cc29d214fd7db2f66da73875e3c9
swalekhan <swalekhan1522@gmail.com>
2023-01-09 10:34:21 +0530

    http-request-inside-component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+let itialValue = true; //we want prevent it on first rendring
+
 function App() {
-  const card = useSelector(state => state.card.cardState);
+  const showCard = useSelector(state => state.card.cardState);
+  const card = useSelector(state => state.cardReducer)
+
+  useEffect(()=>{
+    const sendCardData = async()=>{
+    const response = await fetch("https://redux-project-by-trainer-default-rtdb.firebaseio.com/card.json",{
+      method:"PUT",
+      body:JSON.stringify(card)
+    })
+    if(!response.ok){
+      throw new Error("somthing wrong")
+    }
+     
+  }
+  if(itialValue){
+    itialValue = false;
+    return;
+  }
+    sendCardData().catch((err)=>{
+      console.log(err)
+    })
+  },[card])
+
   return (
     <Layout>
-      {card &&<Cart />}
+      {showCard &&<Cart />}
       <Products />
     </Layout>
   );
